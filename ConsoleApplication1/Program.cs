5df171b1006844659cd669b08298bf6b29fef100
Miguel Angel Torres Renteria <miguelangeltorresrenteri@hotmail.com>
2016-11-17 13:14:53 -0600

    Listing 1-1

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        public static void threadMethod() {
+            for (int i = 0; i < 10; i++ )
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(0);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(threadMethod));
+
+            t.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Main thread: Do some work.");
+                Thread.Sleep(0);
+            }
+
+            t.Join();
+        }
+    }
+}
