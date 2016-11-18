58f293ee970dfc7c9a9c6dc9c0f361305e6f608e
Miguel Angel Torres Renteria <miguelangeltorresrenteri@hotmail.com>
2016-11-18 09:56:22 -0600

    Is created a person class

---
+++ ConsoleApplication1/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Person
+    {
+
+        private String name;
+        private String surname;
+
+        public Person(string name, string surname) {
+            this.name = name;
+            this.surname = surname;
+        }
+
+        public void setName(string name){
+            this.name = name;
+        }
+
+        public void setName(string surname)
+        {
+            this.surname = surname;
+        }
+
+        public string getName() {
+            return name;
+        }
+
+        public string getSurname()
+        {
+            return surname;
+        }
+
+    }
+}
