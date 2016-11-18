7e779e53c1628268c8ef51a6f9afcebac70e7d98
Miguel Angel Torres Renteria <miguelangeltorresrenteri@hotmail.com>
2016-11-18 09:24:24 -0600

    listing 1-3

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,21 +10,21 @@ namespace ConsoleApplication1
     class Program
     {
 
-        public static void threadMethod() {
-            Console.WriteLine("second method");
-            for (int i = 0; i < 10; i++ )
+        public static void threadMethod(object o) {
+            for (int i = 0; i < (int)o; i++ )
             {
                 Console.WriteLine("ThreadProc: {0}", i);
-                Thread.Sleep(0);
+                Thread.Sleep(1000);
             }
         }
 
         public static void Main(string[] args)
         {
-            Thread t = new Thread(new ThreadStart(threadMethod));
-            t.IsBackground = true;
-            t.Start();
-            //Console.WriteLine("main method");
+            Thread t = new Thread(new ParameterizedThreadStart(threadMethod));
+            t.Start(5);
+            Console.WriteLine("main method first line");
+            t.Join();
+            Console.WriteLine("main method second line");
         }
     }
 }
